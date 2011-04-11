45a186d5bf0b8547151d5b340690cabcf59d46a6
Robert Lehmann <mail@robertlehmann.de>
2011-04-11 07:13:11 +0200

    Update example script.

--- example.py
+++ example.py
@@ -1,14 +1,31 @@
+"""
+An Example Presentation
+
+about Faketerm
+"""
+
+__author__ = "John Doe"
+
 with slide("Example slide"):
     print "first bullet point"
     print "second bullet point"
     print "third bullet point"
-with shell():
+with shell() as ctx:
+    ctx.transition = '%'
     print "echo 123"
     print "123"
     print "echo 123"
     print "WOO, SURPRISE! :-)"
-with pyshell():
+    print "python"
+with pyshell() as ctx:
+    ctx.transition = None
     print "1 + 1"
     print "2"
-#with python():
-#    print "40 + 2"
+    print "def foo():\n  pass"
+    print ""
+    print "1 / 0"
+    ctx.throw(ZeroDivisionError("integer division or modulo by zero"))
+with chapter() as ctx:
+    ctx.transition = 3
+    print "Thanks for listening.."
+    print "QUESTIONS?"
